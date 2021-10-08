ff0c5cb18e161b3985059f03c0914bc74944fb5b
Simon Ignacio Leiva Meza <sleivameza@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-10-08 12:48:22 -0300

    incoporacion de metodos predict

--- prob_forc_glm.py
+++ prob_forc_glm.py
@@ -17,6 +17,7 @@ class model(reader):
     def make_features(self, df,
         categ_features = ['currency', 'listing_type', 'shipping_payment'],
         cont_features = ['date', 'current_price', 'minutes_active']):
+        """Method to make de dataset to be used in the fit"""
         y = df[['sold_quantity']]
         X = df[['date', 'current_price', 'currency', 'listing_type', 'shipping_payment', 'minutes_active']]
         X_transf = pd.concat([X[cont_features], 
@@ -26,8 +27,8 @@ class model(reader):
             assign(bias = 1))
         return({'y': y, 'X': X_transf})
 
-    def fit_inidividual(self, sku, model = sm.families.Poisson()):
-        features = self.make_features(self.data.loc[self.data.sku == sku])
+    def fit_inidividual(self, features, model = sm.families.Poisson()):
+        """Method to fit only one SKU or another grouping category"""
         y = features['y']
         X = features['X']
         model_ind = sm.GLM(y,
@@ -35,12 +36,29 @@ class model(reader):
             family = model).fit()        
         return(model_ind)
     
-    def make_future_data(self, sku, days = 15):
+    def make_future_data(self, base_data, days = 15):
+        """Method to make data used to predict"""
+        # Get las row from the base data 
+        last_row_position = base_data.shape[0] - 1
 
-    
-    def predict_individual(self, sku, future_data):
+        future_data = pd.DataFrame({'date': np.arange(np.max(base_data.date) + 1, np.max(base_data.date) + 15 + 1),
+            'current_price': np.repeat(X_transf.current_price[last_row_position], 15),
+            'minutes_active': np.repeat(X_transf.minutes_active[last_row_position], 15),
+            'currency_REA': np.repeat(X_transf.currency_REA[last_row_position], 15),
+            'listing_type_classic': np.repeat(X_transf.listing_type_classic[last_row_position], 15),
+            'shipping_payment_free_shipping': np.repeat(X_transf.shipping_payment_free_shipping[last_row_position], 15),
+            'bias': np.repeat(X_transf.bias[last_row_position], 15)
+        })
 
+        return(future_data)
+        
+    def predict_individual(self, sku):
+        features = self.make_features(self.data.loc[self.data.sku == sku])
+        future_data = self.make_future_data(features['X'])
+        model_in = self.fit_individual(features)
+        ind_preds = model_in.predict(future_data)
 
+        return(ind_preds)
 
 
 
@@ -77,6 +95,17 @@ model_ind = sm.GLM(np.array(y),
     family = sm.families.Poisson()).fit()
 
 model_ind.summary()
+
+
+X_transf
+
+# Expand date
+
+
+
+
+
+
 
 np.array(X_transf)
 
