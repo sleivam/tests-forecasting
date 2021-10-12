3acc8a9859b80af76384f9884e3204c649c17eb2
Simon Ignacio Leiva Meza <sleivameza@internal-admins-internal-traffic-l-6ad629-1124484131.us-east-1.elb.amazonaws.com>
2021-10-12 17:51:09 -0300

    Pruebas y se incorporan predicciones de quantiles y varianza

--- prob_forc_glm.py
+++ prob_forc_glm.py
@@ -3,6 +3,8 @@ import pandas as pd
 import statsmodels.api as sm
 from sklearn.preprocessing import OneHotEncoder as ohe
 import datetime as dt
+from scipy.stats import poisson
+import multiprocessing
 
 class reader:
     """This class is mainly a placeholder for future updates in the data process"""
@@ -27,7 +29,7 @@ class model(reader):
             assign(bias = 1))
         return({'y': y, 'X': X_transf})
 
-    def fit_inidividual(self, features, model = sm.families.Poisson()):
+    def fit_individual(self, features, model = sm.families.Poisson()):
         """Method to fit only one SKU or another grouping category"""
         y = features['y']
         X = features['X']
@@ -41,30 +43,75 @@ class model(reader):
         # Get las row from the base data 
         last_row_position = base_data.shape[0] - 1
 
-        future_data = pd.DataFrame({'date': np.arange(np.max(base_data.date) + 1, np.max(base_data.date) + 15 + 1),
-            'current_price': np.repeat(X_transf.current_price[last_row_position], 15),
-            'minutes_active': np.repeat(X_transf.minutes_active[last_row_position], 15),
-            'currency_REA': np.repeat(X_transf.currency_REA[last_row_position], 15),
-            'listing_type_classic': np.repeat(X_transf.listing_type_classic[last_row_position], 15),
-            'shipping_payment_free_shipping': np.repeat(X_transf.shipping_payment_free_shipping[last_row_position], 15),
-            'bias': np.repeat(X_transf.bias[last_row_position], 15)
+        future_data = pd.DataFrame({'date': np.arange(np.max(base_data.date) + 1, np.max(base_data.date) + days + 1),
+            'current_price': np.repeat(X_transf.current_price[last_row_position], days),
+            'minutes_active': np.repeat(X_transf.minutes_active[last_row_position], days),
+            'currency_REA': np.repeat(X_transf.currency_REA[last_row_position], days),
+            'listing_type_classic': np.repeat(X_transf.listing_type_classic[last_row_position], days),
+            'shipping_payment_free_shipping': np.repeat(X_transf.shipping_payment_free_shipping[last_row_position], days),
+            'bias': np.repeat(X_transf.bias[last_row_position], days)
         })
 
         return(future_data)
         
-    def predict_individual(self, sku):
+    def predict_individual(self, sku, quantile = [0.05, 0.5, 0.95]):
+        """Method to predict one SKU
+        sku: SKU to be predicted
+        quantile: a list with percentiles of prediction"""
         features = self.make_features(self.data.loc[self.data.sku == sku])
-        future_data = self.make_future_data(features['X'])
+        future_data = self.make_future_data(features["X"])
         model_in = self.fit_individual(features)
-        ind_preds = model_in.predict(future_data)
+        in_preds = {}
+        in_preds["means"] = model_in.predict(future_data)
+        # For now the variance is the same as mean, in future updates this should be 
+        # generalized
+        in_preds["variances"] = in_preds["means"]
+        for i in quantile:
+            in_preds["q_" + str(i)] = poisson.ppf(i, mu = in_preds['means'])
+        return(pd.DataFrame(in_preds))
 
-        return(ind_preds)
+
+class mass_model(model):
+    """This class contains the methods to fit and predict all sku in parallel"""
+    def __init__(self, dt_source):
+        super().__init__(dt_source)
+
+    def fit_massive(self):
+        pool = multiprocessing.Pool()
 
 
+
+    
+
+
+
+
+# ========== Pruebas de la clase ==========
 
 data_path = "./tests-forecasting/train_data.parquet"
 
 modelo = model(data_path)
+feat = modelo.make_features(modelo.data.loc[modelo.data.sku == 464801])
+fit_a = modelo.fit_individual(feat)
+future = modelo.make_future_data(feat['X'])
+modelo.predict_individual(464801)
+
+
+
+a['means'] = fit_a.predict(future)
+a['variances'] = a['means']
+quantile = (0.05, 0.5, 0.95)
+
+for i in quantile:
+    a["q_" + str(quantile)] = poisson.ppf(i, mu = means)
+
+
+
+a = {}
+a['ola'] = 5
+a
+fit_a.predict(future)
+
 modelo.fit_inidividual(464801)
 
 
@@ -107,8 +154,3 @@ X_transf
 
 
 
-np.array(X_transf)
-
-encod.n_features_in_
-encod.categories_
-modelo
